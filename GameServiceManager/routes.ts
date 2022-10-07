764216812418b82e05a8ab880ebb7e8224a4edb3
jackal <v@njackal.com>
2022-10-07 12:57:35 -0500

    basic docs and GET / for up status

--- GameServiceManager/routes.ts
+++ GameServiceManager/routes.ts
@@ -13,13 +13,28 @@ type Status = GenericRes & {
     message:string
 }
 
+/**
+ * get the status of a service
+ */
 router.get('/status', (req, res) => {
     res.json({warn:`input query: ${JSON.stringify(req.query)}`})
     res.status(200);
 })
 
+/**
+ * send body {state:bool} and sets the service to that state
+ * returns 409 if the service is already in that state
+ */
 router.post('/state/:id', (req, res) => {
     res.json({warn:`input service id: ${req.params.id}`})
+})
+
+/**
+ * used to determine if the api is online
+ */
+router.get('/', (req,res) => {
+    res.status(200)
+    res.send()
 })
 
 export {
