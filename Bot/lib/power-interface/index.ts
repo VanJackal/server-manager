7215e0600fa021ca693aef014e5ace1efbb30ae7
jackal <v@njackal.com>
2022-11-12 15:43:50 -0600

    initial implementation of power-interface

--- Bot/lib/power-interface/index.ts
+++ Bot/lib/power-interface/index.ts
@@ -1,4 +1,11 @@
 import {logger} from 'logging'
+let gpio;
+if (process.env.PROD){
+    gpio = require("rpi-gpio").promise;
+} else {
+    gpio = null
+}
+
 enum HostState {
     ONLINE,
     OFFLINE,
@@ -13,6 +20,13 @@ type HostStatus = {
 //units should be ms
 const DUR_HOLD = 10000;
 const DUR_PULSE = 500;
+
+const INTERFACE_PIN = 16;// GPIO 23 on RPi (number refers to physical position of the pin)
+
+function init(){
+    gpio.setup(INTERFACE_PIN,gpio.DIR_OUT)
+}
+
 
 async function getState():Promise<HostStatus> {
     logger.warn("Not Implemented - power-interface.getState")
@@ -30,9 +44,27 @@ async function holdPower(){
     return pressPowerButton(DUR_HOLD)
 }
 
-async function pressPowerButton(duration){
-    logger.warn("Not Implemented = power-interface.pressPowerButton")
+async function pressPowerButton(duration):Promise<void>{
+    logger.info(`Power pressed for ${duration}s`)
+    if(!process.env.PROD){
+        logger.warn("PROD env variable not set, no pins fired")
+        return
+    } else {
+        return new Promise(async (resolve, reject) => {
+            await gpio.write(INTERFACE_PIN, true).catch((e) => {
+                reject(e)
+            })
+            setTimeout(() => {
+                gpio.write(INTERFACE_PIN,false).catch((e) => {
+                    reject(e)
+                })
+                resolve()
+            }, duration)
+        })
+    }
 }
+
+if(process.env.PROD) init()
 
 export {
     HostState,
